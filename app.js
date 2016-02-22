90f4f4c53aaa4a90b2f804f72314f4538af4fcb5
Brian Stumbaugh <brian.stumbuagh@gmail.com>
2016-02-22 11:17:23 -0600

    counting url visits now

--- app.js
+++ app.js
@@ -13,18 +13,31 @@ app.set('view engine', 'jade');
 
 app.use(bodyParser.urlencoded({extended:false}));
 
+// redis will keep the session count going, even after the server has been stopped and restarted
 app.use(session({
 	secret: SESSION_SECRET,
     store: new RedisStore()
 }));
 
+// app.use((req, res, next) => {
+//     // this middleware counts the number of sessions. check the terminal, and refresh to watch counter go up.
+//     // even if you add a space somewhere, nodemon will restart the server
+
+//     req.session.count = req.session.count || 0;
+//     req.session.count++;
+//     console.log(req.session);
+//     next();
+// });
+
+// req.url counts how many times you've gone to that same url
+// browse around the site, and check terminal for counts of each url path
 app.use((req, res, next) => {
-    // this middleware counts the number of sessions. check the termina, and refresh to watch counter go up.
-    // even if you add a space somewhere, nodemon will restart the server
-    req.session.count = req.session.count || 0;
-    req.session.count++;
-    console.log(req.session);
-    next();
+      req.session.visits = req.session.visits || {};
+      req.session.visits[req.url] = req.session.visits[req.url] || 0;
+      req.session.visits[req.url]++
+
+      console.log(req.session);
+      next();
 });
 
 
