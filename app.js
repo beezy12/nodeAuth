c581ea7fe72c31fa5aa9e836d2ebef74217e7050
Brian Stumbaugh <brian.stumbuagh@gmail.com>
2016-02-22 10:55:56 -0600

    switching to redis

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
 'use strict';
 
 const express = require('express');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
-const bodyParser = require('body-parser');
+const SESSION_SECRET = process.env.SESSION_SECRET || 'supersecret'
 
 app.set('view engine', 'jade');
 
-app.use(bodyParser.urlencoded({
-	extended:false
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use(session({
+	secret: SESSION_SECRET
 }));
+
+app.use((req, res, next) => {
+    // this middleware counts the number of sessions. check the termina, and refresh to watch counter go up.
+    // even if you add a space somewhere, nodemon will restart the server
+    req.session.count = req.session.count || 0;
+    req.session.count++;
+    console.log(req.session);
+    next();
+});
 
 
 
@@ -30,7 +44,6 @@ app.get('/register', (req, res) => {
 });
 
 app.post('/register', (req, res) => {
-
 	if (req.body.password === req.body.verify) {
 		res.redirect('/login');
 	} else {
@@ -49,3 +62,4 @@ app.post('/register', (req, res) => {
 app.listen(PORT, () => {
 	console.log(`app server is running on port ${PORT}`)
 })
+
