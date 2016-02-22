85db595735baa6bd5f4f984454fdd3cee54a1df9
Brian Stumbaugh <brian.stumbuagh@gmail.com>
2016-02-22 09:41:43 -0600

    login working

--- app.js
+++ app.js
@@ -6,9 +6,19 @@ const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'jade');
 
+
+
 app.get('/', (req, res) => {
 	res.render('index');
-})
+});
+
+app.get('/login', (req, res) => {
+	res.render('login');
+});
+
+app.post('/login', (req, res) => {
+	res.redirect('/')
+});
 
 
 
