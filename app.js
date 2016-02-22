44fb27632533bbe4d950b52e317444e07172e964
Brian Stumbaugh <brian.stumbuagh@gmail.com>
2016-02-22 09:47:34 -0600

    switching to login branch

--- app.js
+++ app.js
@@ -3,8 +3,13 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
 
 app.set('view engine', 'jade');
+
+app.use(bodyParser.urlencoded({
+	extended:false
+}));
 
 
 
